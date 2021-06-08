d3cf54472a9e4dc3487e007681e8bcbf2a7aa91f
Pijus Kamandulis <pkpjuklas@gmail.com>
2021-06-08 19:56:04 +0300

    Use go modules

--- main.go
+++ main.go
@@ -5,60 +5,61 @@ import (
 	"fmt"
 	"path/filepath"
 
-	. "./fileio"
-	. "./helpers"
-	. "./structs"
 	"github.com/mmcdole/gofeed"
+
+	fileio "github.com/pikami/rss-dl/fileio"
+	helpers "github.com/pikami/rss-dl/helpers"
+	structs "github.com/pikami/rss-dl/structs"
 )
 
 func main() {
-	GetConfig()
+	structs.GetConfig()
 
 	fp := gofeed.NewParser()
-	LogInfo("Downloading " + Config.FeedURL)
-	feed, _ := fp.ParseURL(Config.FeedURL)
+	helpers.LogInfo("Downloading " + structs.Config.FeedURL)
+	feed, _ := fp.ParseURL(structs.Config.FeedURL)
 
-	outputDir := Config.OutputPath + "/" + ToCleanString(feed.Title)
-	InitOutputDirectory(outputDir)
+	outputDir := structs.Config.OutputPath + "/" + helpers.ToCleanString(feed.Title)
+	fileio.InitOutputDirectory(outputDir)
 
 	feedInfoPath := outputDir + "/feed_details.json"
-	LogInfo("Writing feed details as JSON to " + feedInfoPath)
-	WriteToFile(feedInfoPath, GrabFeedDetailsJSON(feed))
+	helpers.LogInfo("Writing feed details as JSON to " + feedInfoPath)
+	fileio.WriteToFile(feedInfoPath, GrabFeedDetailsJSON(feed))
 
-	feedImagePath := outputDir + "/image" + RemoveGetParams(filepath.Ext(feed.Image.URL))
-	DownloadFile(feedImagePath, feed.Image.URL)
+	feedImagePath := outputDir + "/image" + helpers.RemoveGetParams(filepath.Ext(feed.Image.URL))
+	fileio.DownloadFile(feedImagePath, feed.Image.URL)
 
 	for _, item := range feed.Items {
-		itemOutputFilename := ToCleanString(
+		itemOutputFilename := helpers.ToCleanString(
 			item.PublishedParsed.Format("20060102") + "_" + item.Title)
 		itemOutputDir := outputDir + "/" + itemOutputFilename
 
-		if CheckIfExists(itemOutputDir) {
+		if fileio.CheckIfExists(itemOutputDir) {
 			fmt.Println("Item '" + item.Title + "' already downloaded, skipping")
 			continue
 		}
 
-		LogInfo("Downloading feed item '" + item.Title + "' to " + itemOutputDir)
-		InitOutputDirectory(itemOutputDir)
+		helpers.LogInfo("Downloading feed item '" + item.Title + "' to " + itemOutputDir)
+		fileio.InitOutputDirectory(itemOutputDir)
 
 		itemDetailsPath := itemOutputDir + "/details.json"
-		LogInfo("Writing details to " + itemDetailsPath)
-		WriteToFile(
+		helpers.LogInfo("Writing details to " + itemDetailsPath)
+		fileio.WriteToFile(
 			itemDetailsPath,
 			GrabFeedItemJSON(item))
 
 		if item.Image != nil {
-			itemImagePath := itemOutputDir + "/image" + RemoveGetParams(filepath.Ext(item.Image.URL))
-			LogInfo("Downloading image to " + itemImagePath)
-			DownloadFile(
+			itemImagePath := itemOutputDir + "/image" + helpers.RemoveGetParams(filepath.Ext(item.Image.URL))
+			helpers.LogInfo("Downloading image to " + itemImagePath)
+			fileio.DownloadFile(
 				itemImagePath,
 				item.Image.URL)
 		}
 
 		for _, enclosure := range item.Enclosures {
-			filename := RemoveGetParams(filepath.Base(enclosure.URL))
-			LogInfo("Downloading attachment '" + filename + "'")
-			DownloadFile(
+			filename := helpers.RemoveGetParams(filepath.Base(enclosure.URL))
+			helpers.LogInfo("Downloading attachment '" + filename + "'")
+			fileio.DownloadFile(
 				itemOutputDir+"/"+filename,
 				enclosure.URL)
 		}
@@ -67,7 +68,7 @@ func main() {
 
 // GrabFeedDetailsJSON - Returns a feed summary json sring
 func GrabFeedDetailsJSON(feed *gofeed.Feed) string {
-	feedParsed := &FeedDetails{
+	feedParsed := &structs.FeedDetails{
 		Title:       feed.Title,
 		Description: feed.Description,
 		Categories:  feed.Categories,
@@ -83,7 +84,7 @@ func GrabFeedDetailsJSON(feed *gofeed.Feed) string {
 
 // GrabFeedItemJSON - Returns a feed summary json sring
 func GrabFeedItemJSON(item *gofeed.Item) string {
-	itemParsed := &ItemDetails{
+	itemParsed := &structs.ItemDetails{
 		Title:       item.Title,
 		Description: item.Description,
 		Content:     item.Content,
